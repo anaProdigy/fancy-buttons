837e04333f24b2439ec915b4817f8b46bb010222
Anastasia <z.anastasia11@gmail.com>
2023-06-08 22:22:41 -0700

    create components for 4 buttons

---
+++ src/components/CounterButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CounterButton = () => {
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+}
+
+export default CounterButton;
